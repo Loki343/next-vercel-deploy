3d5a25611f5c93bf57e4f315152d51509349aa86
Kausik Bhaumik <kausikbhaumik42@gmail.com>
2023-01-08 00:28:13 +0530

    done all

---
+++ pages/counter.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Counter() {
+    const [counter,setCounter] = useState(0)
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={()=>setCounter(counter+1)}>INC</button>
+      <button onClick={()=>setCounter(counter-1)}>DEC</button>
+    </div>
+  )
+}
+
+export default Counter
