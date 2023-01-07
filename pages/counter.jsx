9731c82896c2328301960388fedd5ad1076fae36
Kausik Bhaumik <kausikbhaumik42@gmail.com>
2023-01-08 00:34:23 +0530

    new

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -4,7 +4,7 @@ function Counter() {
     const [counter,setCounter] = useState(0)
   return (
     <div>
-      <h1>Counter: {counter}</h1>
+      <h1>Count: {counter}</h1>
       <button onClick={()=>setCounter(counter+1)}>INC</button>
       <button onClick={()=>setCounter(counter-1)}>DEC</button>
     </div>
